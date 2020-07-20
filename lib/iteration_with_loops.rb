6cf1ed2e6813ebc45cebad2cfda84a627d28652e
Matthew Kirby <matthew.robbins.kirby@gmail.com>
2020-07-20 19:29:46 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,13 +5,13 @@ def find_min_in_nested_arrays(src)
   min_temperature = []
   src.length.times do |row_index|
     
+    row_min_temp = src[index][0]
     src[row_index].length.times do |element_index|
-      
+      if src[row_index][element_index] < row_min_temp
+        row_min_temp = src
+      end
     end
     
   end
   
 end
-var test
-puts test
-puts 10 > test
